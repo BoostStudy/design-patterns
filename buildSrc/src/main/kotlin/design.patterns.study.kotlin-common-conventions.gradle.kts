b47b031e22f5cc835320f1f7a3aac6543f321dcb
ezhoon <dlwlgns1240@gmail.com>
2024-02-12 21:55:13 +0900

    Coroutine 의존성 추가

--- buildSrc/src/main/kotlin/design.patterns.study.kotlin-common-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/design.patterns.study.kotlin-common-conventions.gradle.kts
@@ -13,6 +13,10 @@ dependencies {
 
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.3")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // coroutine
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.5.2")
 }
 
 tasks.named<Test>("test") {
